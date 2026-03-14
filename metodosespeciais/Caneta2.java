a895998fea010d11ab1de1b442071058160047c0
6Sixfrosty <galaxyslayer0009@gmail.com>
2026-03-14 15:14:12 -0300

    Aula de Métodos Especiais em java

---
+++ metodosespeciais/Caneta2.java
@@ -0,0 +1,12 @@
+package metodosespeciais;
+
+public class Caneta2 {
+    public static void main(String[] args) {
+       // Moldecaneta c1 = new Moldecaneta();
+       // c1.setModelo("BIC");
+       // c1.setPonta(0.5);
+       Moldecaneta2 c1 = new Moldecaneta2("BIC", "Azul", 0.5f);
+
+       c1.status();
+    }
+}
---
+++ metodosespeciais/Moldecaneta2.java
@@ -0,0 +1,35 @@
+package metodosespeciais;
+
+public class Moldecaneta2 {
+    private String modelo;
+    private float ponta;
+    private String cor;
+    private boolean tampada;
+
+    public Moldecaneta2() {}
+
+    public Moldecaneta2 (String m, String c, float p) 
+    {
+        this.modelo = m;
+        this.cor = c;
+        this.ponta = p;
+        this.tampar();
+    }
+
+    public String getModelo() { return this.modelo; }
+    public void setModelo (String m) { this.modelo = m; }
+
+    public float getPonta () { return ponta;}
+    public void setPonta (float p) { this.ponta = p; }
+
+    public void tampar() { this.tampada = true; }
+    public void destampar() { this.tampada = false; }
+
+    public void status() {
+        System.out.println("Sobre a caneta: ");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta : " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " +  this.tampada);
+    }
+}
