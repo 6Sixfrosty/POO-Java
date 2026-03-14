a398273776df72a304688029d06acada4fbcb4d2
6Sixfrosty <galaxyslayer0009@gmail.com>
2026-03-14 14:00:05 -0300

    Pratica de classes e objetos em java

---
+++ Classes e objetos/Caneta.java
@@ -0,0 +1,12 @@
+public class Caneta {
+    public static void main(String[] args) {
+        Moldecaneta c1 = new Moldecaneta();
+        c1.modelo = "BIC Cristal";
+        c1.cor = "Azul";
+        //c1.ponta = 0.5f;
+        c1.carga = 80;
+        c1.destampar();
+        c1.status();
+        c1.rabiscar();
+    }
+}
---
+++ Classes e objetos/Moldecaneta.java
@@ -0,0 +1,33 @@
+public class Moldecaneta{
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        if(this.tampada == true)
+            System.out.println("Está tampada?  Sim" );
+        else 
+            System.out.println("Está tampada?  Não");
+    }
+
+    public void rabiscar(){
+        if (this.tampada == true)
+            System.out.println("ERRO! Não posso rabiscar");
+        else 
+            System.out.println("Estou Rabiscando");
+    }
+
+    protected  void tampar(){
+        this.tampada = true;
+    }
+
+    protected void destampar(){
+        this.tampada = false;
+    }
+}
